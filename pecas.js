37534b6d80edc0a07d73491bba1894bd4fc27672
kmylas <kammylas2005@gmail.com>
2021-12-12 22:12:43 -0300

    Criando sistema de peças

---
+++ pecas.js
@@ -0,0 +1,27 @@
+var listaDePecas = ["Filtro de Ar","Motor","Amortecedor"]
+
+if (listaDePecas.length < 10) {
+// É possível cadastrar
+    console.log("É possível cadastrar mais peças")
+} else {
+    console.log("Não tem mais espaço na lista")
+}
+
+let peso = 50;
+if(peso<100){
+    console.log("A peça deve pesar no mínimo 100g")
+}else {
+    console.log("A peça possui peso adequado")
+}
+
+let nomePeca = "Disco de Freio"
+if (nomePeca.length>3){
+    console.log("Nome de peça está adequado para o cadastro")
+}else if(nomePeca.length ==0){
+    console.log("O nome da peça não pode ser vazio")
+}else{
+    console.log("O nome deve ter mais de 3 caracteres, digite um nome adequado")
+}
+
+
+
